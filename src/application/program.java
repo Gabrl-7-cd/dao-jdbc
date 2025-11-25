6a7f242a15788ec990720efb84b4c9b6255accab
GabrielSA <ga5227221@gmail.com>
2025-11-25 12:47:55 -0300

    update implementation

--- src/application/program.java
+++ src/application/program.java
@@ -30,9 +30,15 @@ public class program {
 			System.out.println(obj);
 		}
 		System.out.println("\n=== TEST 4: seller insert =====");
-		Seller newSeller = new Seller(null, "Haito", "haito@gmail.com", new Date(), 4000.0, department);
+		Seller newSeller = new Seller(null, "Junior", "junior@gmail.com", new Date(), 3500.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Feyernord");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
